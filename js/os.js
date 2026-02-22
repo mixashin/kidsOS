56240c01d311b1bb90816678affb2597954148d5
mixashin <mixashin@gmail.com>
2026-02-22 22:19:15 +0100

    Update sound effects and placeholder video

--- js/os.js
+++ js/os.js
@@ -1,6 +1,6 @@
 /* ===== KidsOS Core ===== */
 const OS = (() => {
-  const VERSION = '0.6.5';
+  const VERSION = '0.6.6';
   const UPDATE_URL = (typeof KIDSOS_CONFIG !== 'undefined' && KIDSOS_CONFIG.updateURL) || 'http://localhost:5555';
 
   let zCounter = 100;
