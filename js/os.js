8d0b9c3b8503b6079b48eebcdf973c4e30ceaea5
mixashin <mixashin@gmail.com>
2026-02-22 17:27:56 +0100

    Mobile icons: 3-column grid with 2x larger icons
    Switched from 4-col to 3-col grid. Icon emoji scaled to 72px on
    tablet, 60px on mobile, 52px on small phone. Labels scaled up too.

--- js/os.js
+++ js/os.js
@@ -1,6 +1,6 @@
 /* ===== KidsOS Core ===== */
 const OS = (() => {
-  const VERSION = '0.4.5';
+  const VERSION = '0.4.6';
   const UPDATE_URL = 'https://lena.mixorium.net';
 
   let zCounter = 100;
