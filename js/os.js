3d97c0697e107adbe03eeb24761c9bd4b3ebb564
mixashin <mixashin@gmail.com>
2026-02-24 21:34:19 +0100

    Bump version to 0.24.1

--- js/os.js
+++ js/os.js
@@ -1,6 +1,6 @@
 /* ===== KidsOS Core ===== */
 const OS = (() => {
-  const VERSION = '0.24.0';
+  const VERSION = '0.24.1';
   const UPDATE_URL = (typeof KIDSOS_CONFIG !== 'undefined' && KIDSOS_CONFIG.updateURL) || 'https://mixashin.github.io/kidsOS';
 
   let zCounter = 100;
